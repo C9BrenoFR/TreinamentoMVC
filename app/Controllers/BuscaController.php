66163d115274e6bc718bc814b3fe9833675ebe11
BrenoFR <breno.rosado@estudante.ufjf.com.br>
2024-12-07 18:24:54 -0300

    Busca + Paginação

--- app/Controllers/BuscaController.php
+++ app/Controllers/BuscaController.php
@@ -8,8 +8,53 @@ use Exception;
 class BuscaController
 {
 
+    private const ITENS_PER_PAGE = 5;
+
     public function index()
     {
-        return view('site/busca');
+
+        if(isset($_GET['search'])){
+            $total_itens = App::get('database')->countWithSearch('posts', 'titulo', $_GET['search']);
+            $total_pages = ceil($total_itens/BuscaController::ITENS_PER_PAGE);
+
+            if(
+            isset($_GET['page']) 
+            && filter_var($_GET['page'], FILTER_VALIDATE_INT) 
+            && $_GET['page'] > 0 
+            && $_GET['page'] <= $total_pages
+            )
+                $page = $_GET['page'];
+            else 
+                $page = 1;
+
+            $skip = ($page - 1) * BuscaController::ITENS_PER_PAGE;
+
+            $posts = App::get('database')->selectAllWhithSearch('posts', 'titulo', $_GET['search'], BuscaController::ITENS_PER_PAGE, $skip);
+            $search = "&search=" . $_GET['search'];
+            return view('site/busca', compact('posts', 'page', 'total_pages', 'search'));
+        }
+        
+        $total_itens = App::get('database')->count('posts');
+        $total_pages = ceil($total_itens/BuscaController::ITENS_PER_PAGE);
+
+        if(
+        isset($_GET['page']) 
+        && filter_var($_GET['page'], FILTER_VALIDATE_INT) 
+        && $_GET['page'] > 0 
+        && $_GET['page'] <= $total_pages
+        )
+            $page = $_GET['page'];
+        else 
+            $page = 1;
+
+        $skip = ($page - 1) * BuscaController::ITENS_PER_PAGE;
+
+        $posts = App::get('database')->selectAll("posts", BuscaController::ITENS_PER_PAGE, $skip);
+        return view('site/busca', compact('posts', 'page', 'total_pages'));
+
     }
+
+
+
+
 }
--- app/views/site/busca.view.php
+++ app/views/site/busca.view.php
@@ -3,9 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/css/style.css">
     <title>Busca</title>
 </head>
 <body>
-    
+    <div class="container">
+        <form action="/busca" method="get" class="container-in">
+            <input name="search" type="text" required placeholder="Busque aqui!">
+        </form>
+        <div class="container-in">
+            <?php foreach($posts as $post): ?>
+            <div>
+                <h1><?= $post->titulo ?></h1>
+                <p><?= $post->conteudo ?></p>
+            </div>
+            <?php endforeach; ?>
+        </div>
+
+        <div class="paginate">
+            <a href="/busca?page=<?= $page == 1 ? $page : $page-1 ?><?= isset($search) ? $search : "" ?>"> < </a>
+            <?php for($index = 1; $index <= $total_pages; $index++): ?>
+            <a href="/busca?page=<?= $index ?><?= isset($search) ? $search : "" ?>"><?= $index ?></a>
+            <?php endfor; ?>
+            <a href="/busca?page=<?= $page == $total_pages ? $page : $page+1 ?><?= isset($search) ? $search : "" ?>"> > </a>
+        </div>
+    </div>
 </body>
 </html>
